9dbe8d7b0e831dae06fb6d0e12fe3c5277c99d71
Allison Pedro <allisondjkk@gmail.com>
2021-07-25 17:20:14 -0300

    seletor-cores-arquivos-css-grid

---
+++ javascript/color.js
@@ -0,0 +1,21 @@
+document.querySelector("#btn-laranja").addEventListener("click", function(){
+    document.querySelector(".container").setAttribute("class","bg-laranja container")
+      
+})
+
+
+
+document.querySelector("#btn-vermelho").addEventListener("click", function(){
+    document.querySelector(".container").setAttribute("class","bg-vermelho container")
+})
+
+
+
+document.querySelector("#btn-purpura").addEventListener("click", function(){
+    document.querySelector(".container").setAttribute("class","bg-purpura container")
+})
+
+
+document.querySelector("#btn-gradiente").addEventListener("click", function(){
+    document.querySelector(".container").setAttribute("class","bg-gradiente container")
+})
